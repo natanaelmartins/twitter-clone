6f5f11dad047c0840fc16a16a2b3b64501716465
natanaelmartins <70239432+natanaelmartins@users.noreply.github.com>
2022-06-27 23:34:58 -0300

    Update index.tsx

--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 
 /* altere o Tweet */
 import Tweet from '../Tweet';
-import db from 'firebase';
+import db from 'src/firebase.js';
 
 /*
 import Post from './Post';
