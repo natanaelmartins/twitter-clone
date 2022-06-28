78089111178a6d5c4077ba78c9141fcb1ba8d818
natanaelmartins <70239432+natanaelmartins@users.noreply.github.com>
2022-06-27 23:11:02 -0300

    Update index.tsx

--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
+/* por enquanto, tudo ok */
+import React, { useState, useEffect } from 'react';
 
+/* altere o Tweet */
 import Tweet from '../Tweet';
+import db from './firebase';
+
+/*
+import Post from './Post';
+import './Feed.css';
+import FlipMove from "react-flip-move";
+*/
 
 import { Container, Tab, Tweets } from './styles';
 
+/*
+function Feed () {
+  const [posts, setPosts] = useState([]);
+  
+  useEffect(() => {
+    db.collection("posts").onSnapshot((snapshot) =>
+      setPosts(snapshot.docs.map((doc) => doc.data()))
+    );
+  }, []);
+  
+  return (
+*/
+  
 const Feed: React.FC = () => {
   return(
     <Container>
