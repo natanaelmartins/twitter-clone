484f23c183918edf36e33c70844c37be24a10f0a
Natanael Martins <70239432+natanaelmartins@users.noreply.github.com>
2022-08-16 19:58:02 -0300

    Importei o ícone `close`
    Para usar na seção `EditarPerfil`

--- src/styles/Icons.ts
+++ src/styles/Icons.ts
@@ -6,6 +6,7 @@ export {
   ExitToApp,
   LocationOn,
   Search,
+  Close,
 } from 'styled-icons/material-outlined';
 export { Person, Cake, Favorite } from 'styled-icons/material';
 export { ArrowLeft } from 'styled-icons/heroicons-solid';
