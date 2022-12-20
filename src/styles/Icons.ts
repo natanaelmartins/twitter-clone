30f7704eac481c922a07e37d1dc52b7d01313ce8
Natanael Martins <70239432+natanaelmartins@users.noreply.github.com>
2022-12-19 22:46:04 -0300

    Adding the `AddAPhoto` icon

--- src/styles/Icons.ts
+++ src/styles/Icons.ts
@@ -7,11 +7,14 @@ export {
   LocationOn,
   Search,
   Close,
+  AddAPhoto
 } from 'styled-icons/material-outlined';
 export { Person, Cake, Favorite } from 'styled-icons/material';
 export { ArrowLeft } from 'styled-icons/heroicons-solid';
 export { Chat } from 'styled-icons/heroicons-outline';
-export { Twitter } from 'styled-icons/icomoon'
-export { Retweet } from 'styled-icons/entypo'
-/* Aqui em baixo, exporto o ícone criado na pasta RocketseatIcon */ 
+export { Twitter } from 'styled-icons/icomoon';
+export { Retweet } from 'styled-icons/entypo';
+/* 
+Aqui em baixo, exporto o ícone criado na pasta RocketseatIcon 
 export { RocketseatIcon as Rocketseat } from '../components/RocketseatIcon';
+*/
