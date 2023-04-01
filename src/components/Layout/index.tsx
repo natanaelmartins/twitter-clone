21fcd7c6ddda77647666b97ed1a472befebcf3e5
Natanael Martins <70239432+natanaelmartins@users.noreply.github.com>
2023-04-01 11:23:50 -0300

    adding createContext

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 
 import MenuBar from '../MenuBar';
 import SideBar from '../SideBar';
@@ -6,13 +6,43 @@ import Main from '../Main';
 
 import { Container, Wrapper } from './styles';
 
+interface MessageContextInterface {
+  username: string;
+  setUsername: React.Dispatch<React.SetStateAction<string>>;
+  bio: string;
+  setBio: React.Dispatch<React.SetStateAction<string>>;
+  avatar: File | null | undefined;
+  setAvatar: React.Dispatch<React.SetStateAction<File | null>>;
+  header: File | null | undefined;
+  setHeader: React.Dispatch<React.SetStateAction<File | null>>;
+}
+
+export const MessageContext = React.createContext<MessageContextInterface>({
+  username: "",
+  setUsername: () => {},
+  bio: "",
+  setBio: () => {},
+  avatar: null,
+  setAvatar: () => {},
+  header: null,
+  setHeader: () => {},
+});
+
 const Layout: React.FC = () => {
+  
+  const [username, setUsername] = useState("Natanael Martins");
+  const [bio, setBio] = useState("Estudante de Sistemas de Informação :)");
+  const [avatar, setAvatar] = useState<File | null>(null);
+  const [header, setHeader] = useState<File | null>(null);
+  
   return (
     <Container>
       <Wrapper>
-        <MenuBar /> 
-          <Main />
-        <SideBar />
+        <MessageContext.Provider value={{ username, setUsername, bio, setBio, avatar, setAvatar, header, setHeader }}>
+          <MenuBar />
+            <Main />
+          <SideBar />
+        </MessageContext.Provider>
       </Wrapper>
     </Container>
   );
